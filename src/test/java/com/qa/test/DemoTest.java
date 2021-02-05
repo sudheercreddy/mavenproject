1613d7e2af45d760be95467f207dfff312885907
schittireddy <sudheerqa.selenium@gmail.com>
2021-02-04 19:10:41 -0600

    first commit

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,37 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void sum() {
+		int a=10;
+		int b=20;
+		int c=30;
+		Assert.assertEquals(30, a+b);
+		System.out.println("Add is :" + (a+b));
+	}
+	@Test
+	public void sub() {
+		int d=30;
+		int e=31;
+		Assert.assertEquals(-1, d-e);
+		System.out.println("Sub is :" +  (d-e));
+	}
+	@Test
+	public void mul() {
+		int d=30;
+		int e=31;
+		Assert.assertEquals(930, d*e);
+		System.out.println("Mul is :" +  (d*e));
+	}
+	@Test
+	public void div() {
+		int d=5;
+		int e=5;
+		Assert.assertEquals(1, 5/5);
+		System.out.println("Div is :" +  (d/e));
+	}
+
+}
